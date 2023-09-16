15ad8eaaf185ece684954182836e3dfe4af40b8c
Andres More <moreandres@users.noreply.github.com>
2023-09-15 22:15:09 -0300

    Update whois.py

--- whois/whois.py
+++ whois/whois.py
@@ -353,6 +353,8 @@ class NICClient(object):
             return 'whois.nic.tv'            
         elif tld == 'bz':
             return NICClient.IANAHOST
+        elif tld == 'ky':
+            return 'whois.uniregistrar.com'
         elif tld == 'mw':
             return 'whois.nic.mw'            
         elif tld == 'city':
